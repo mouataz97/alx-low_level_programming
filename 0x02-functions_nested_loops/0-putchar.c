e1e40d6eaf83bf47b580ac806573b88c33e7861e
El Mouataz Billah Fachatti <mouatazbillah97@gmail.com>
2023-06-20 16:50:33 -0700

    m

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,26 +1,20 @@
-/*
- * File: 0-putchar.c 
- *
+#include "main.h"
+/**
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
  */
 
- #include "main.h" 
-
- /** 
- * main-print putchar followed by a new line
- * main.h 
- *Return :always 0
- */
-  int main(void) 
- {
-      _putchar('_' );
-      _putchar ('p' );
-      _putchar ('u' );
-      _putchar ('t' );
-      _putchar ('c' );
-      _putchar ('h' );
-      _putchar ('a' );
-      _putchar ('r' );
-      _putchar ('\n');
-      
-       return (0) ;
-} 
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return(0);
+}
