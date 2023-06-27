b50672f060db6ea3af5e68b2d7c2e0685e751697
El Mouataz Billah Fachatti <mouatazbillah97@gmail.com>
2023-06-27 15:58:16 -0700

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,13 +2,13 @@
 
 /**
  *  _strlen - Returns the length of a string.
- *  @str: The string to get the length of 
+ *  @s: pointer to the string to check
  *  Return: The length of @str.
  */
 int _strlen(char *s)
 {
 	int i = 0;
-	while (s[i])
+	while (s[i]) 
 		i++;
 
 	return (i);
