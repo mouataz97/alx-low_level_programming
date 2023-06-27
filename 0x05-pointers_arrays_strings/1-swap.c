3f6a8a0a4a201a01d9596a100ff4cda1e9126535
El Mouataz Billah Fachatti <mouatazbillah97@gmail.com>
2023-06-27 15:39:31 -0700

    task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,10 @@
 #include "main.h"
 /**
- * function that swaps the values of two integers.
- * DarkVlad
- *
  * swap_int - swaps the values of two intergers.
-   * @a: The first interger to be swapped.
-   * @b: The second interger to be swapped.
-   *
-   * Return: nothing
+  * @a: The first interger to be swapped.
+  * @b: The second interger to be swapped.
+  *
+  * Return: nothing
  */
 void swap_int(int *a, int *b)
 {
