12d122e028ed3e7460ea7c6efefbd11a32fdc4bb
El Mouataz Billah Fachatti <mouatazbillah97@gmail.com>
2023-06-27 16:15:26 -0700

    task 3

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -8,11 +8,10 @@
 int _strlen(char *s)
 {
 	int c = 0;
-
 	for (; *s != '\0'; s++)
 	{
 		c++;
 	}
-
 	return (c);
 }
+
--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,11 +8,11 @@
   */
 void _puts(char *str)
 {
-	 while (*str != '\0' )
-	 {
-		 _putchar(*str);
-		 str++;
-	 }
-	 _putchar('\n');
+	while (*str != '\0' )
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
 }
 
