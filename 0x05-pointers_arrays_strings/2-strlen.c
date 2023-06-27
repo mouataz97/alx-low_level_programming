424460b6bd220ee179da3617d3dd753ff0125adc
El Mouataz Billah Fachatti <mouatazbillah97@gmail.com>
2023-06-27 15:42:31 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *  _strlen - Returns the length of a string.
+ *  @str: The string to get the length of 
+ *  Return: The length of @str.
+ */
+int _strlen(char *s)
+{
+	return strlen(*s);
+}
+
