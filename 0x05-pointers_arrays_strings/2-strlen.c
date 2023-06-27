c16150eea3972b5fd52744fe7f91b30243f8a500
El Mouataz Billah Fachatti <mouatazbillah97@gmail.com>
2023-06-27 16:07:26 -0700

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
-/** 
-  * _strlen - find the length of a string 
-  * @s: pointer to the string to check 
+/**
+  * _strlen - find the length of a string
+  * @s: pointer to the string to check
   *Return: void
   */
 int _strlen(char *s)
