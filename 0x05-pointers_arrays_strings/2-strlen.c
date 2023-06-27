f8e154f820d12fe10950aa1fec196e652a4fc43f
El Mouataz Billah Fachatti <mouatazbillah97@gmail.com>
2023-06-27 16:06:21 -0700

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,11 +5,14 @@
   * @s: pointer to the string to check 
   *Return: void
   */
-int _strlen(char *s) 
+int _strlen(char *s)
 {
-   int i = 0;
-   while (s[i]) 
-   	i++;
+	int c = 0;
 
-   return (i);
-} 
+	for (; *s != '\0'; s++)
+	{
+		c++;
+	}
+
+	return (c);
+}
