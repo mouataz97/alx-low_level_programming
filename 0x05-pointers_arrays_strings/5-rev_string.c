c79e5ce3dc196c1d9a9614bb289e3637b43b5ccd
El Mouataz Billah Fachatti <fachatti.mouataz.billah@gmail.com>
2023-07-01 05:30:50 -0700

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
-/** 
+/**
  * rev_string - Write a function that reverses a string
- * 
- * @s: string var pointer 
+ *
+ * @s: string var pointer
  */
 void rev_string(char *s)
 {
 	int i, j, temp;
-	i= 0;
-	j= 0;
+	i = 0;
+	j = 0;
 
 	while (s[i] != '\0')
 	{
 		j++;
 	}
 	j--;
-	while (j>i)
+	while (j > i)
 	{
 		temp = s[j];
 		s[j--] = s[i];
