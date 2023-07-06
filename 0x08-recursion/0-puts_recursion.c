5b43bd7bb22edcbb33111971fd3007827e9d6bc7
El Mouataz Billah Fachatti <fachatti.mouataz.billah@gmail.com>
2023-07-06 03:32:50 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
-*_my_puts_recursion - prints a string recursively
-*@s: input string
-*Return: void
-*/
-void _my_puts_recursion(char *s)
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Always 0 (Success)
+ */
+
+void _puts_recursion(char *s)
 {
 	if (*s)
 	{
-		_my_putchar(*s);
-		_my_puts_recursion(s + 1);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
+
 	else
-	{
-		_my_putchar('\n');
-	}
+		_putchar('\n');
 }
+
